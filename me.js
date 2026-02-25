dfcf03e4038870663e5a70664d562e861914393f
awangerin2445 <adrian.wangerin2445@gmail.com>
2026-02-25 11:04:54 -0600

    Add files via upload

---
+++ me.js
@@ -0,0 +1,15 @@
+//Adrian All about me 2/25/2026
+
+let myname = "Adrian";
+let favColor = "Blue";
+let favhobby = "gaming";
+let favFood = "Tacos";
+let favAnimal = "Capybara";
+
+//Display information
+console.log("My name is:", myname);
+console.log("My favorite color is:" + favColor);
+console.log('My favorite hobby is: ${favHobby}');
+console.log('My favorite food is:' + favFood);
+console.log('My favorite animal is' + favAnimal); 
+
